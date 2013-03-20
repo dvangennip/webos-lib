df79ac036c214d484812aa934d2e069773fd994b
dvangennip <vangennip@gmail.com>
2013-03-20 15:09:23 +0100

    lowercase webos fixes some reference bugs
    while webos-ext used webOS for its references, this has been changed to
    webos in webos-lib.

--- source/webos.js
+++ source/webos.js
@@ -143,4 +143,10 @@ enyo.requiresWindow(function() {
 		};
 	}
 });
+<<<<<<< HEAD
 enyo.webos = webos;
+=======
+webOS = window.webos;
+enyo.webOS = webOS;
+enyo.webos = enyo.webOS;
+>>>>>>> lowercase webos fixes some reference bugs
