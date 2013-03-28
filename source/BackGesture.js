ce64fb07896a89bc25a8f5903b4f7e061bfc71a3
Doménique van Gennip <domenique@sinds1984.nl>
2013-03-28 16:05:40 +0100

    Include Back identifier and send along event
    Older webOS versions use the Back keyIdentifier which was missed
    before. The key event is send along so any event handler can use
    stopPropagation and preventDefault functions of the event object.

--- source/BackGesture.js
+++ source/BackGesture.js
@@ -1,16 +1,18 @@
 /**
 	
-	Add an event listener for keyup to document to listen for the custom "U+1200001" key on OwOS
-	or for the ESC key (U+001B) on other platforms and call onbackbutton to be compatible with
-	PhoneGap. According to (http://forums.enyojs.com/discussion/comment/5826/#Comment_5826) webOS 2.x
-	gives a "Back" key identifier.
+	Add an event listener for keyup to document to listen for the custom "U+1200001" key on OwOS,
+	"Back" key on webOS 2.x or for the ESC key (U+001B) on other platforms and call onbackbutton
+	to be compatible with PhoneGap.
+
+	When handling the onbackbutton event, inEvent.stopPropagation() and/or inEvent.preventDefault()
+	are supported to stop the default behavior.
 	
 */
 
 (function() {
 	enyo.dispatcher.listen(document, 'keyup', function(ev) {
 		if (ev.keyIdentifier == "U+1200001" || ev.keyIdentifier == "U+001B" || ev.keyIdentifier == "Back") {
-			enyo.Signals.send('onbackbutton');
+			enyo.Signals.send('onbackbutton', ev);
 		}
 	});
 })();
